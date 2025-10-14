665fe277d33e0e84d1cd1303baabf39e4e35534b
Marina_2.0 <chursina0121@gmail.com>
2025-10-14 21:45:42 +0300

    Изминен хост

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,7 @@ const adminRoutes = require('./routes/admin');
 const local = "localhost:3000"
 const server= "http://91.229.9.244:3000"
 const app = express();
-app.get('/', (req, res) => {
-  res.send('✅ Backend is alive!');
-});
+
 app.use('/images', express.static('public/images'));
 
 // Настройка CORS с поддержкой cookies
@@ -54,6 +52,9 @@ app.use('/auth', authRoutes);
 app.use('/child', childRoutes);
 app.use('/teacher', teacherRoutes);
 app.use('/admin', adminRoutes);
+app.get('/', (req, res) => {
+  res.send('✅ Backend is alive!');
+});
 
 const PORT = 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`✅ Server running on port ${PORT}`));
